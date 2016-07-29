191221b18549643699cc69487c92bfd5b94f6724
bg <602566046@qq.com>
2016-07-29 18:32:25 +0800

    Initial commit

---
+++ src/main/java/com/bg/NettyWebFilestransferApplication.java
@@ -0,0 +1,12 @@
+package com.bg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyWebFilestransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyWebFilestransferApplication.class, args);
+	}
+}
---
+++ src/main/java/com/bg/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.bg.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2016/7/29.
+ */
+@Controller
+public class HomeController {
+
+}
